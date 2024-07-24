c8d17e5396b0959f419be198e3b854afbeba2c06
Oscar Escobar Ulloa <osescobar92@gmail.com>
2024-07-24 13:41:12 -0400

    Validate day, month and year of endTime
    Create three tests for the cases:
    - The day of endTime should be the same of startTime.
    - The month of endTime should be the same of startTime.
    - The year of endTime should be the same of startTime.

--- src/appointment/appointment.service.spec.ts
+++ src/appointment/appointment.service.spec.ts
@@ -65,4 +65,48 @@ describe('AppointmentService', () => {
     })
     .toThrowError("appointment's endTime should be after startTime");
   });
+
+  it('should throw an error when end time is in the next day', () => {
+    const startTime = new Date('2022-01-01T14:00:00Z');
+    const endTime = new Date('2022-01-02T00:00:00Z');
+
+    expect(() => {
+        service.scheduleAppointment({
+            patientId: 1,
+            startTime,
+            endTime
+        });
+    })
+    .toThrowError("appointment's endTime should be in the same day as start time's");
+  });
+
+  it('should throw an error when end time is in same day and hour of next month', () => {
+    const startTime = new Date('2022-01-01T14:00:00Z');
+    const endTime = new Date('2022-02-01T14:00:00Z');
+    
+    expect(() =>
+      service.scheduleAppointment({
+        patientId: 1,
+        startTime,
+        endTime,
+      })
+    ).toThrowError(
+      "appointment's endTime should be in the same day as start time's"
+    );
+  });
+
+  it('should throw an error when end time is in same day, hour and month of the next year', () => {
+    const startTime = new Date('2022-01-01T14:00:00Z');
+    const endTime = new Date('2023-01-01T14:00:00Z');
+    
+    expect(() =>
+      service.scheduleAppointment({
+        patientId: 1,
+        startTime,
+        endTime,
+      })
+    ).toThrowError(
+      "appointment's endTime should be in the same day as start time's"
+    );
+  });
 });
--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -14,9 +14,32 @@ export class AppointmentService {
             throw new Error("appointment's endTime should be after startTime");
         }
         
+        if (this.endTimeIsInTheNextDay(appointmentData)) {
+            throw new Error(
+              "appointment's endTime should be in the same day as start time's",
+            );
+        }
+
         return {
             ...appointmentData,
             confirmed: false
         }
+    }
+
+    private endTimeIsInTheNextDay(appointmentData: AppointmentInput): boolean {
+        const differentDays = 
+            appointmentData.endTime.getUTCDate() !==
+            appointmentData.startTime.getUTCDate();
+
+        const differentMonths = 
+            appointmentData.endTime.getUTCMonth() !==
+            appointmentData.startTime.getUTCMonth();
+
+        // Now we also check for years
+        const differentYears =
+        appointmentData.endTime.getUTCFullYear() !==
+        appointmentData.startTime.getUTCFullYear();
+
+        return differentDays || differentMonths || differentYears;
     }
 }
