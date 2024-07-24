4bc15890f485ac2ac451c8b0b2bdedf21741f36b
Oscar Escobar Ulloa <osescobar92@gmail.com>
2024-07-24 13:07:30 -0400

    validate the endTime should de after startTime
    Create the the test 'should throw an error when the end time is equal to
    start time' and the implementation for passing it.

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -10,7 +10,7 @@ export interface AppointmentInput {
 @Injectable()
 export class AppointmentService {
     public scheduleAppointment(appointmentData: AppointmentInput): Appointment {
-        if(appointmentData.endTime < appointmentData.startTime) {
+        if(appointmentData.endTime <= appointmentData.startTime) {
             throw new Error("appointment's endTime should be after startTime");
         }
         
