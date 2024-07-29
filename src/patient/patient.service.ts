f818c7d3d83854875ebbc31db4c6921b7995f07c
Oscar Escobar Ulloa <osescobar92@gmail.com>
2024-07-29 11:47:16 -0400

    create the register patient feature

---
+++ src/patient/patient.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { Patient } from './patient.model';
+
+interface PatientInput {
+    name: string;
+}
+
+@Injectable()
+export class PatientService {
+
+    async register(patientInput: PatientInput): Promise<Patient> {
+        return {
+            id: 1,
+            name: patientInput.name
+        };    
+    } 
+}
